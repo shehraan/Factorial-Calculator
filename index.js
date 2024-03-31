c959f0b4633d4cdacc6a53af3fd18b0f98d806d9
Shehraan Hafiz <72216883+shehraan@users.noreply.github.com>
2024-03-30 22:46:28 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+  let input = parseInt(prompt("Number please: "));
+  let i = 1
+  while (i<=input && i > 0 ) {
+  for (i = 1 ; i <= input; i++){
+  answer = input * i 
+  }
+  console.log("The factorial is : " + answer)
+  }
